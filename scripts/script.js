654f45cec898cc8b349705a7c4b5878aacdf036a
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-10-12 12:47:04 -0600

    ocultando cosas en gitignore

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('mostrarCuriosidades').addEventListener('click', function() {
       var curiosidades = document.getElementById('curiosidades');
       if (curiosidades.style.display === 'none' || curiosidades.style.display === '') {
-        curiosidades.style.display = 'block';  // Mostrar
+        curiosidades.style.display = 'block';  // show
       } else {
-        curiosidades.style.display = 'none';  // Ocultar
+        curiosidades.style.display = 'none';  // hide
       }
     });
   });
